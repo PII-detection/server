4efceab0d45f57dbfb6874cfb4ca5cc2c1c84a01
sweetspot00 <50688000+sweetspot00@users.noreply.github.com>
2022-04-30 19:53:19 +0800

    py36

--- crowdstrike_PII_detection/settings.py
+++ crowdstrike_PII_detection/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pl4oucltvu3z8lxfu8lb9rnuj!9*uw0f855_0+yjl1!u+7x-w5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pii-detection.herokuapp.com']
+ALLOWED_HOSTS = ['pii-detection.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
